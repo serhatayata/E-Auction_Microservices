86aa8c7222a452df819c984b598a13abf5433c72
Serhat Ayata <srhtayata@gmail.com>
2021-12-09 14:42:21 +0300

    Ordering.Domain - Entities - Generic

---
+++ Ordering.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Entities.Base
+{
+    public abstract class Entity : IEntityBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int Id { get; protected set; } //protected Id nesnesine, Entity içinden sadece ulaşılabilir.
+
+        public Entity Clone()
+        {
+            return (Entity)this.MemberwiseClone();
+        }
+    }
+
+
+
+
+
+}
---
+++ Ordering.Domain/Repositories/Base/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using Ordering.Domain.Entities;
+using Ordering.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Repositories.Base
+{
+    public interface IRepository<T> where T:Entity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate=null,
+                                      Func<IQueryable<T>,IOrderedQueryable<T>> orderby=null, //burada Orderby yapılmak istenen
+                                      string includeString = null, //Bu kısımda lazy loading için kullanılır
+                                      bool disableTracking=true //Entity framework performance optimizer yaptığımız parametresi.
+                                     );
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T Entity);
+        Task UpdateAsync(T Entity);
+        Task DeleteAsync(T Entity);
+
+    }
+}
