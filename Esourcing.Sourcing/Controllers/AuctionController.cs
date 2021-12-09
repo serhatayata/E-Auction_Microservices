61155dc9eb45ee06db7e6264a425428bd2114b61
Serhat Ayata <srhtayata@gmail.com>
2021-12-09 11:02:07 +0300

    Startup RabbitMQ Singleton DI - EventBusConstants

--- Esourcing.Sourcing/Controllers/AuctionController.cs
+++ Esourcing.Sourcing/Controllers/AuctionController.cs
@@ -1,6 +1,8 @@
 ﻿using Esourcing.Sourcing.Data.Abstract;
 using Esourcing.Sourcing.Entities;
 using Esourcing.Sourcing.Repositories.Abstract;
+using Esourcing.Sourcing.Repositories.Concrete;
+using EventBusRabbitMQ.Events.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -18,25 +20,27 @@ namespace Esourcing.Sourcing.Controllers
     {
         private readonly IAuctionRepository _auctionRepository;
         private readonly ILogger<AuctionController> _logger;
-        public AuctionController(IAuctionRepository auctionRepository,ILogger<AuctionController> logger)
+        private readonly IBidRepository _bidRepository;
+        public AuctionController(IAuctionRepository auctionRepository,IBidRepository bidRepository, ILogger<AuctionController> logger)
         {
             _auctionRepository = auctionRepository;
-            _logger=logger;
+            _logger = logger;
+            _bidRepository = bidRepository;
         }
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<Auction>),(int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(IEnumerable<Auction>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<IEnumerable<Auction>>> GetAuctions()
         {
             var auctions = await _auctionRepository.GetAuctions();
             return Ok(auctions);
         }
-        [HttpGet("{id:length(24)}",Name ="GetAuction")]
+        [HttpGet("{id:length(24)}", Name = "GetAuction")]
         [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<ActionResult<Auction>> GetAuction(string id)
         {
             var auction = await _auctionRepository.GetAuction(id);
-            if (auction ==null)
+            if (auction == null)
             {
                 _logger.LogError($"Auction with id : {id} hasn't been found in the database.");
                 return NotFound();
@@ -48,7 +52,7 @@ namespace Esourcing.Sourcing.Controllers
         public async Task<ActionResult<Auction>> CreateAuction([FromBody] Auction auction)
         {
             await _auctionRepository.Create(auction);
-            return CreatedAtRoute("GetAuction", new { id=auction.Id });
+            return CreatedAtRoute("GetAuction", new { id = auction.Id });
         }
 
         [HttpPut]
@@ -65,6 +69,32 @@ namespace Esourcing.Sourcing.Controllers
             return Ok(await _auctionRepository.Delete(id));
         }
 
+        [HttpPost("CompleteAuction")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> CompleteAuction(string id)
+        {
+            Auction auction = await _auctionRepository.GetAuction(id);
+            if (auction ==null)
+            {
+                return NotFound();
+            }
+            if (auction.Status!=(int)Status.Active)
+            {
+                _logger.LogError("Auction can not be completed.");
+                return BadRequest();
+            }
 
+            Bid bid = await _bidRepository.GetWinnerBid(id);
+            if (bid==null)
+            {
+                return NotFound();
+            }
+            
+            
+            
+
+
+        }
     }
 }
--- Esourcing.Sourcing/Startup.cs
+++ Esourcing.Sourcing/Startup.cs
@@ -3,6 +3,8 @@ using Esourcing.Sourcing.Data.Concrete;
 using Esourcing.Sourcing.Repositories.Abstract;
 using Esourcing.Sourcing.Repositories.Concrete;
 using Esourcing.Sourcing.Settings;
+using EventBusRabbitMQ;
+using EventBusRabbitMQ.Producer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -52,6 +55,37 @@ namespace Esourcing.Sourcing
                     Version = "v1"
                 });
             });
+            #endregion
+            #region EventBus
+            // NORMALDE 
+            services.AddSingleton<IRabbitMQPersistentConnection, DefaultRabbitMQPersistentConnection>();
+            //
+            services.AddSingleton<IRabbitMQPersistentConnection>(sp => {
+                var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+                var factory = new ConnectionFactory() {
+                    HostName = Configuration["EventBus:HostName"],                    
+                };
+                if (!string.IsNullOrWhiteSpace(Configuration["EventBus:UserName"]))
+                {
+                    factory.UserName = Configuration["EventBus:UserName"];
+                }
+                if (!string.IsNullOrWhiteSpace(Configuration["EventBus:Password"]))
+                {
+                    factory.UserName = Configuration["EventBus:Password"];
+                }
+                var retryCount = 5;
+                if (!string.IsNullOrWhiteSpace(Configuration["EventBus:RetryCount"]))
+                {
+                    retryCount = int.Parse(Configuration["EventBus:RetryCount"]);
+                }
+
+                return new DefaultRabbitMQPersistentConnection(factory,retryCount,logger);
+
+
+            });
+
+            services.AddSingleton<EventBusRabbitMQProducer>();
+
             #endregion
 
         }
